cc6379421deb4964e7492cb64e7c9b4926383ff0
Alexey Shmelkov <ashmelkov@aligntech.com>
2019-10-01 13:06:01 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,50 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
+const DECODER = {
+ '10': '.',
+ '11': '-',
+ '**********': ' '
+}
+
 function decode(expr) {
-    // write your solution here
+    let lettersInDigits = expr.match(/.{10}/g) 
+    let symbolsToLettersMap = new Map(Object.entries(MORSE_TABLE));
+    let digitsToSymbolsMap = new Map(Object.entries(DECODER))
+
+    //Separate digits to 2 length or 10 '*'
+    separatedDigitsArr = []
+    lettersInDigits.forEach(separateDigits);
+
+    function separateDigits(item) {
+        separatedDigitsArr.push(item.match(/\d{2}|[*]{10}/g))
+    }
+
+    // Map digits with morse symbols
+    symbols = []
+    separatedDigitsArr.forEach(mapSymbols)
+
+    function mapSymbols(item) {
+        symbols.push(item.map(digitsToSymbols).join(''))
+    }
+    
+    function digitsToSymbols(digits) {
+        return digitsToSymbolsMap.get(digits)
+    }
+   
+    //Map morse symbols with letters
+    letters = []
+    symbols.forEach(mapLetters)
+
+    function mapLetters(item) {
+        letters.push(symbolsToLettersMap.get(item))
+    }
+
+    return letters.join('');
+    
 }
 
 module.exports = {
